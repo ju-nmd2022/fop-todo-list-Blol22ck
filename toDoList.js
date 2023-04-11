143b2d407552a025e5efccbe5ef6e8a4f36a305b
Blol22ck <117266908+Blol22ck@users.noreply.github.com>
2023-04-11 21:09:33 +0200

    Trying to make the button work with the input

--- toDoList.js
+++ toDoList.js
@@ -1,39 +1,56 @@
-const contentElement = document.getElementById("content");
-const setTaskElement = document.getElementById("addTask");
-let currentTask;
+// const contentElement = document.getElementById("content");
+// const setTaskElement = document.getElementById("addTask");
+// let currentTask;
+
+// let taskList = [];
+
+// function addTaskList() {
+//   setTaskElement.innerHTML = "";
+
+//   if (currentTask) {
+//     setTaskElement.innerText = "" + currentTask;
+
+//     const checkButton = document.createElement("button");
+//     checkButton.innertext = "Check";
+//     setTaskElement.appendChild(checkButton);
+//     checkButton.addEventListener("click", () => {
+//       currentTask = undefined;
+//       addTaskList();
+//     });
+//   } else {
+//     const inputElement = document.createElement("input");
+//     inputElement.placeholder = "Write Task";
+//     setTaskElement.appendChild(inputElement);
+
+//     const removeButton = document.createElement("button");
+//     removeButton.innerText = "Remove";
+//     setTaskElement.appendChild(removeButton);
+//     removeButton.addEventListener("click", () => {
+//       if (inputElement.value.length > 0) {
+//         currentTask = inputElement.value;
+//         addTaskList();
+//       }
+//     });
+//   }
+// }
+
+const tasks = [""];
+const taskListElement = document.getElementById("addTask");
+const taskCartElement = document.getElementById("taskCart");
 
 let taskList = [];
 
-//function displayTasks() {
-//
-//}
+for (let task of tasks) {
+  const taskElement = document.createElement("input");
+  taskElement.innerText = task;
+  taskElement.onclick = addToTaskList;
+  taskListElement.appendChild(taskElement);
+}
 
-function addTaskList() {
-  setTaskElement.innerHTML = "";
+function addToTaskList() {
+  taskList.push(this.innerText);
 
-  if (currentTask) {
-    setTaskElement.innerText = "" + currentTask;
-
-    const checkButton = document.createElement("button");
-    checkButton.innertext = "Check";
-    setTaskElement.appendChild(checkButton);
-    checkButton.addEventListener("click", () => {
-      currentTask = undefined;
-      addTaskList();
-    });
-  } else {
-    const inputElement = document.createElement("input");
-    inputElement.placeholder = "Write Task";
-    setTaskElement.appendChild(inputElement);
-
-    const removeButton = document.createElement("button");
-    removeButton.innerText = "Remove";
-    setTaskElement.appendChild(removeButton);
-    removeButton.addEventListener("click", () => {
-      if (inputElement.value.length > 0) {
-        currentTask = inputElement.value;
-        addTaskList();
-      }
-    });
-  }
+  const cartElement = document.createElement("div");
+  cartElement.innertext = this.innerText;
+  taskCartElement.appendChild(cartElement);
 }
